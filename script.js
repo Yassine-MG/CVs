449225ed1e1d5bb25216816c496304e291bfcf29
Yassine MGHAZLI <yassinemghazli03@gmail.com>
2022-12-25 20:35:24 +0100

    Commit

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let typewriter = new Typewriter(app, {
     loop: true
 });
 // import './node_modules/animejs/lib/anime.js'
-typewriter.typeString('<strong>Web </strong>')
+typewriter.typeString('<strong>Web</strong>')
     .pauseFor(2000)
     .deleteChars(4)
     .typeString('<strong>Mobile</strong>')
